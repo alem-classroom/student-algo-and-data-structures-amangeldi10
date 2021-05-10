f25ca98e191c0c3de0714365a769838eae304c22
amangeldi10 <41150730+amangeldi10@users.noreply.github.com>
2021-05-10 10:35:59 +0600

    Update linear_search.py

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,7 +1,7 @@
 def linear_search(lst, to_find):
   a=-1
-  if x in lst:
-    a=lst.index(x)
+  if to_find in lst:
+    a=lst.index(to_find)
   return a
   # search for the element to_find inside lst
   # if found, return index of element
